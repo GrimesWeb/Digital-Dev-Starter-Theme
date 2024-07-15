3cb76d666ba77461984eee53e4448a350f55595e
The Digital Dev <dgrimesbusiness@gmail.com>
2024-07-15 14:28:51 -0400

    initial start to load template

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php if(have_posts()) : while(have_posts()) : the_post();?>
+
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <?php the_content(); ?>
+    <hr>
+<?php endwhile; else: ?>
+
+<?php endif; ?>
